9fefd0f38b5193deac94c7751df3ff830f002c27
Zangar Tasbolat <82701005+Zangar003@users.noreply.github.com>
2024-05-15 14:49:05 +0500

    Update main.go

--- main.go
+++ main.go
@@ -680,8 +680,7 @@ func WatchPost(w http.ResponseWriter, r *http.Request) {
 
 // google login
 var googleOauthConfig = &oauth2.Config{
-	// RedirectURL:  "http://localhost:8000/auth/google/callback",
-	RedirectURL:  os.Getenv("GOOGLE_OAUTH_REDIRECT_URL"),
+	RedirectURL:  "http://localhost:8000/auth/google/callback",
 	ClientID:     os.Getenv("GOOGLE_OAUTH_CLIENT_ID"),
 	ClientSecret: os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET"),
 	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
