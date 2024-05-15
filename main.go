9f4958d297fd2a036e1c41ec3dc235f2dbb35483
Zangar <200103376@stu.sdu.edu.kz>
2024-05-15 14:21:27 +0500

    last 3

--- main.go
+++ main.go
@@ -680,7 +680,8 @@ func WatchPost(w http.ResponseWriter, r *http.Request) {
 
 // google login
 var googleOauthConfig = &oauth2.Config{
-	RedirectURL:  "http://localhost:8000/auth/google/callback",
+	// RedirectURL:  "http://localhost:8000/auth/google/callback",
+	RedirectURL:  os.Getenv("GOOGLE_OAUTH_REDIRECT_URL"),
 	ClientID:     os.Getenv("GOOGLE_OAUTH_CLIENT_ID"),
 	ClientSecret: os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET"),
 	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
